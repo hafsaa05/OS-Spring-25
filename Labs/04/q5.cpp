04888e90ab5e7808ea351f6d48eeab31b5a7c456
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2025-02-21 06:07:55 +0500

    Create q5.cpp

---
+++ Labs/04/q5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+using namespace std;
+
+int main() {
+    pid_t pid;
+
+    pid = fork();
+    if (pid == 0) {
+        execlp("ls", "ls", "-l", (char *)NULL);
+        cerr << "Error: Failed to execute ls command." << endl;
+        return 1;
+    } 
+    else if (pid > 0) {
+        wait(NULL);
+        cout << "Child process completed. Parent process terminating." << endl;
+    } 
+    else {
+        cerr << "Error: Failed to create child process." << endl;
+    }
+    return 0;
+}
