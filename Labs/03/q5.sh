4c12fd9949260777d7384c1bd938836111a0b7a6
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2025-03-02 08:45:41 +0500

    Create q5.sh

---
+++ Labs/03/q5.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <directory_path>"
+    exit 1
+fi
+
+source_dir=$1
+if [ ! -d "$source_dir" ]; then
+    echo "Error: Directory not found!"
+    exit 1
+fi
+
+backup_dir="backup_$(date +%Y-%m-%d_%H-%M-%S)"
+mkdir "$backup_dir"
+
+cp -r "$source_dir"/* "$backup_dir"
+
+file_count=$(find "$backup_dir" -type f | wc -l)
+dir_count=$(find "$backup_dir" -type d | wc -l)
+
+echo "Backup completed!"
+echo "Backup folder: $backup_dir"
+echo "Files: $file_count, Directories: $((dir_count - 1))"
+
