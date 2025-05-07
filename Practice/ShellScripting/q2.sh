f5d074960114764bf678983ac64a32d414988b2b
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2025-05-07 05:05:15 +0500

    Create q2.sh

---
+++ Practice/ShellScripting/q2.sh
@@ -0,0 +1,16 @@
+#Write a script that accepts a sentence and counts the number of vowels in it.
+#!/bin/bash
+
+echo "Enter a sentence:"
+read sentence  
+
+vowels=0  
+
+for (( i=0; i<${#sentence}; i++ )); do
+    char=${sentence:$i:1}  
+    case "$char" in
+        [aeiouAEIOU]) ((vowels++)) ;;  
+    esac
+done
+
+echo "Number of vowels: $vowels"  
